6a4f11edebff19f5fd2969bfd5bbe2654f5944e5
wapa <Madouylliez@gmail.com>
2014-04-03 10:03:05 +0200

    tp7 done

---
+++ tp7/raytrace_Douylliez/src/application/Sphere.cpp
@@ -0,0 +1,66 @@
+#include "Sphere.h"
+#include <cmath>
+#include <iostream>
+#include "GLTool.h"
+#include "IntersectionArray.h"
+
+/**
+@file
+@author Fabrice Aubert
+*/
+
+using namespace p3d;
+using namespace std;
+
+Sphere::Sphere() : Primitive() {}
+
+
+/**
+  donne la liste croissante issus de l'intersection de ray=(A,u)  et de la sphere x^2+y^2=1
+  (2 ou 0 intersection)
+*/
+void Sphere::intersection(const Ray &ray,IntersectionArray *result) {
+
+    /**
+   * A COMPLETER : il faut résoudre l'équation en lambda, puis créer dans le résultat les intersections correspondantes aux lambda  :
+   * !! pour ajouter une intersection à la fin de la liste result, utilisez uniquement : result->addIntersection(lambda)
+   * ray est déjà dans le repère local de la sphere unitaire centrée à l'origine :
+   *   - ray.point() : donne l'origine du rayon (Vector3)
+   *   - ray.direction() : donne le vecteur directeur (Vector3)
+   * il faut impérativement retourner 0 ou 2 intersections pour assurer une cohérence dans l'intersection.
+   * il faut que les intersections soient triées (ordre croissant) pour assurer le fonctionnement avec l'algorithme général d'intersection avec le CSG
+   *
+   *  rappel : a.dot(b) donne le produit scalaire de a par b
+   */
+
+    result->clear(); // initialisation : liste d'intersection vide
+
+    Vector3 A = ray.point();
+    Vector3 u = ray.direction();
+    double delta = (2*A.dot(u))*(2*A.dot(u)) - (4*u.dot(u)*(A.dot(A)-1));
+    if (delta >0){
+        double inter1,inter2;
+        inter1 = ((-2*A.dot(u))-sqrt(delta))/(2*u.dot(u));
+        inter2 = ((-2*A.dot(u))+sqrt(delta))/(2*u.dot(u));
+        result->addIntersection(inter1);
+        result->addIntersection(inter2);
+    }
+}
+
+
+/** ****************************************************** **/
+/** ****************************************************** **/
+/** ****************************************************** **/
+/** ****************************************************** **/
+
+Vector3 Sphere::computeNormal(const Vector3 &p) {
+    return p;
+}
+
+
+void Sphere::drawGL() {
+    p3d::shaderLightPhong();
+    materialGL();
+    p3d::drawSphere();
+}
+
